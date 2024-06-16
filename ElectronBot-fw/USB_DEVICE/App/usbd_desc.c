9dfd806da51a59ca20355a5b6113c66a60755d5a
gil zhang <gil.zhang.dev@outlook.com>
2024-06-16 21:51:35 +0800

    微调代码

--- ElectronBot-fw/USB_DEVICE/App/usbd_desc.c
+++ ElectronBot-fw/USB_DEVICE/App/usbd_desc.c
@@ -186,7 +186,7 @@ __ALIGN_BEGIN uint8_t USBD_HS_DeviceDesc[USB_LEN_DEV_DESC] __ALIGN_END =
   0x00,                       /*bcdUSB */
 #endif /* (USBD_LPM_ENABLED == 1) */
 
-  0x00,                       /*bDeviceClass*/
+  0x02,
   0x00,                       /*bDeviceSubClass*/
   0x00,                       /*bDeviceSubClass*/
   0x00,                       /*bDeviceProtocol*/
