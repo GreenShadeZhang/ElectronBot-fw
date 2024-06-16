431e20c33b41bda33b75f434679d2a9c48622046
gil zhang <gil.zhang.dev@outlook.com>
2024-06-16 16:04:55 +0800

    测试原版的配置

--- ElectronBot-fw/USB_DEVICE/Target/usbd_conf.h
+++ ElectronBot-fw/USB_DEVICE/Target/usbd_conf.h
@@ -76,6 +76,8 @@
 /*---------- -----------*/
 #define USBD_SELF_POWERED     1U
 
+#define USBD_SUPPORT_WINUSB       1
+
 /****************************************/
 /* #define for FS and HS identification */
 #define DEVICE_FS 		0
